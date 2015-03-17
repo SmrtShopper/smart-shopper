978c71da981b5c296435187a91a0989b94e36ad5
jclaw <jclaw3456@gmail.com>
2015-03-17 12:30:35 -0400

    added basic files and experimenting with angular.js

---
+++ app.js
@@ -0,0 +1,12 @@
+(function() {
+	var app = angular.module('smartShopper', [ ]);
+
+	app.controller('ListController', function(){
+		this.product = item;
+	});
+
+	var item = {
+		description: "Ice cream",
+		price: 14.95,
+	}
+})();
