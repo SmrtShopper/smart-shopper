17acc651f840a09a295d73f1b3919637b472ba4a
mseltzer94 <michael.m.seltzer@gmail.com>
2015-03-18 02:07:28 -0600

    add button, attempt to change color when bought

--- item.js
+++ item.js
@@ -1,6 +1,10 @@
 angular.module('smartShopper', [])
   .controller('GroceryController', ['$scope', function($scope) {
     $scope.items = [
-      {description:'ice cream', price:14.95},
-      {description:'milk', price:3.49}];
+      {description:'ice cream', price:14.95, bought: true},
+      {description:'milk', price:3.49, bought: false}];
+    $scope.addItem = function() {
+        $scope.items.push({description: $scope.groceryItem, price:1000000, bought: false});
+        $scope.groceryItem = '';
+    };
   }]);
