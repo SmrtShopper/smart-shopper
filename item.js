c807e6d2acde322974c39e219f3bdd20fc35f4fd
mseltzer94 <michael.m.seltzer@gmail.com>
2015-03-18 02:11:34 -0600

    aisle badge, fixed color if bought

--- item.js
+++ item.js
@@ -1,8 +1,8 @@
 angular.module('smartShopper', [])
   .controller('GroceryController', ['$scope', function($scope) {
     $scope.items = [
-      {description:'ice cream', price:14.95, bought: true},
-      {description:'milk', price:3.49, bought: false}];
+      {description:'ice cream', price:14.95, bought: true, aisle: 3},
+      {description:'milk', price:3.49, bought: false, aisle: 5}];
     $scope.addItem = function() {
         $scope.items.push({description: $scope.groceryItem, price:1000000, bought: false});
         $scope.groceryItem = '';
