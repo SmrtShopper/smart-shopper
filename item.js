94894c8ba9cc041ce5caeff12f0a4f26de01630d
mseltzer94 <michael.m.seltzer@gmail.com>
2015-03-18 14:32:15 -0600

    manually allow for changes to voice recognized item string

--- item.js
+++ item.js
@@ -4,7 +4,7 @@ angular.module('smartShopper', [])
       {description:'ice cream', price:14.95, bought: true, aisle: 3},
       {description:'milk', price:3.49, bought: false, aisle: 5}];
     $scope.addItem = function() {
-        $scope.items.push({description: $scope.groceryItem, price:1000000, bought: false});
+        $scope.items.push({description: document.getElementById("grocItem").value, price:1000000, bought: false});
         $scope.groceryItem = '';
     };
     $scope.voiceRec = function(){
@@ -12,8 +12,10 @@ angular.module('smartShopper', [])
     recognition.onresult = function(event) { 
       console.log(event) 
       if (event.results[0].isFinal) {
-      //document.getElementById("grocItem").value = event.results[0][0].transcript;
-          $scope.items.push({description: event.results[0][0].transcript, price:1000000, bought: false});
+          //add to textbox
+          document.getElementById("grocItem").value = event.results[0][0].transcript;
+          //auto add? probably don't want to do that
+          //$scope.items.push({description: $scope.groceryItem, price:1000000, bought: false});
       }
     }
     recognition.onerror = function(event){
