16a1ac8fa4f36dc372c1aa444f4acd29120e214c
jclaw <jclaw3456@gmail.com>
2015-04-09 03:05:54 -0400

    added remove icon

--- assets/js/groceryItem.js
+++ assets/js/groceryItem.js
@@ -4,25 +4,27 @@ angular.module('smartShopper', [])
       {description:'ice cream', bought: true, quantity: 3},
       {description:'milk', bought: false, quantity: 5}];
     $scope.prependItem = function() {
-        $scope.items.unshift({description: document.getElementById("grocItem").value, bought: false});
-        document.getElementById("grocItem").value = '';
+      $scope.items.unshift({description: document.getElementById("grocItem").value, bought: false});
+      document.getElementById("grocItem").value = '';
     };
+
+
     $scope.voiceRec = function(){
         
-    var recognition = new webkitSpeechRecognition();
-    recognition.onresult = function(event) { 
-      console.log(event) 
-      if (event.results[0].isFinal) {
-          //add to textbox
-          document.getElementById("grocItem").value = event.results[0][0].transcript;
-          //auto add? probably don't want to do that
-          //$scope.items.push({description: $scope.groceryItem, price:1000000, bought: false});
+      var recognition = new webkitSpeechRecognition();
+      recognition.onresult = function(event) { 
+        console.log(event) 
+        if (event.results[0].isFinal) {
+            //add to textbox
+            document.getElementById("grocItem").value = event.results[0][0].transcript;
+            //auto add? probably don't want to do that
+            //$scope.items.push({description: $scope.groceryItem, price:1000000, bought: false});
+        }
       }
-    }
-    recognition.onerror = function(event){
-        console.log("Error", event);   
-    }
-    recognition.start();
-    };
-  }]);
+      recognition.onerror = function(event){
+          console.log("Error", event);   
+      }
+      recognition.start();
+      };
+    }]);
 
