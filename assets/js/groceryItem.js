a0e6b276df5c7ab7d2fc1e36258285b9d9128cd0
jclaw <jclaw3456@gmail.com>
2015-04-17 09:49:36 -0400

    changed prependItem to prependToList

--- assets/js/groceryItem.js
+++ assets/js/groceryItem.js
@@ -8,13 +8,33 @@ angular.module('smartShopper', [])
       {description: 'cheese'},
       {description: 'broccoli'}
     ];
+
+    $scope.search = function() {
+      
+      var query = document.getElementById("grocItem").value;
+      //search Nutritionix for search results...
+
+      document.getElementById("grocItem").value = '';
+    }
+
+    $scope.prependToList = function(idx) {
+      var item = $scope.searchResults[idx];
+
+      if ($scope.items.indexOf(item) == -1) {
+        $scope.items.unshift(item);
+      }
+      else {
+        alert("Oops, that's already in your list!");
+      }
+
+    /*
     $scope.prependItem = function() {
       var item_title = document.getElementById("grocItem").value;
       if (item_title != "") {
               $scope.items.unshift({description: item_title, bought: false});
       document.getElementById("grocItem").value = '';
       }
-
+    */
     };
     $scope.deleteItem = function(idx) {
       $scope.items.splice(idx, 1);
