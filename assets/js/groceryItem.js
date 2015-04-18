f8ab3a1c0dea786a433c5ba4eb1cfca96ba7f73d
mseltzer94 <michael.m.seltzer@gmail.com>
2015-04-18 04:10:33 -0400

    CHARTS

--- assets/js/groceryItem.js
+++ assets/js/groceryItem.js
@@ -9,12 +9,19 @@ angular.module('smartShopper', ["chart.js"])
     //   {fields:{item_name: 'broccoli'}}
     // ];
 
-    $scope.items = JSON.parse(localStorage.getItem("grocery")) || [];
-    $scope.allitemstring = '';
+    $scope.alldata = JSON.parse(localStorage.getItem("grocery")) || [];
 
     $scope.search = function() {
       
       var query = document.getElementById("grocItem").value;
+      var allitemstr = query + "\n";
+      if ($scope.alldata.results) {
+        for (var i = 0; i < $scope.alldata.results.length; i++) {
+        allitemstr += $scope.alldata.results[i].parsed_query.query + "\n";
+        }
+      }
+      
+      console.log(allitemstr);
       var items_to_return = 3;
       var appId = "feab83eb";
       var appKey = "ecc75d64bf6a77ba3f03d478d4ee943e";
@@ -26,43 +33,60 @@ angular.module('smartShopper', ["chart.js"])
       xmlhttp.setRequestHeader("Content-Type", "text/plain");
       xmlhttp.onreadystatechange=function() {
         if (xmlhttp && xmlhttp.readyState == 4 && xmlhttp.status == 200){
-          console.log(JSON.parse(xmlhttp.responseText).results[0]);
-          item = JSON.parse(xmlhttp.responseText).results[0]; 
-          $scope.items.unshift(item);
-          $scope.allitemstring+="\n" + query;
+          console.log(JSON.parse(xmlhttp.responseText));
+          alldata = JSON.parse(xmlhttp.responseText); 
+          $scope.alldata = alldata;
           $scope.$apply();
-          console.log($scope.items);
-          localStorage.setItem("grocery", JSON.stringify($scope.items));
+          console.log($scope.alldata.results);
+          localStorage.setItem("grocery", JSON.stringify($scope.alldata));
         }
+
       }
-     xmlhttp.send(query);
+      xmlhttp.send(allitemstr);
+
+      // var allitemstr;
+      // for (var i = 0; i < $scope.items.length; i++) {
+      //   allitemstr += "\n" + $scope.items[i].parsed_query.food;
+      // }
+      //xmlhttp.onreadystatechange=function() 
+      
+      //xmlhttp.send(allitemstr);
+
+      //query = 
+      $scope.updateGraphs();
+
 
       document.getElementById("grocItem").value = '';
     };
 
-    
-    // console.log($scope.items);
-    // var item = $scope.items[0];
-    // console.log(item);
-    // var labels = [];
-    // var data = [];
+    $scope.updateGraphs = function(){
+      var total = $scope.alldata.total;
+      var labels = [];
+      var data = [];
 
 
-    // for (var i = 0; i < item.nutrients.length; i++) {
-    //   if (item.nutrients[i].unit == "g") {
-    //     labels.splice(i, 0, item.nutrients[i].name);
-    //     data.splice(i, 0, item.nutrients[i].value);
-    //   }
-    // }
-    // console.log(data);
+      for (var i = 0; i < total.nutrients.length; i++) {
+        if (total.nutrients[i].unit == "g") {
+          labels.push(total.nutrients[i].name);
+          data.push(total.nutrients[i].value);
+        }
+      }
+      console.log(total);
 
-    // $scope.labels = labels;
-    // $scope.data = data;
+      $scope.labels = labels;
+      $scope.data = data;
+    }
+
+    $scope.$on('$viewContentLoaded', function() {
+      $scope.updateGraphs();
+    });
+   
 
 
     $scope.deleteItem = function(idx) {
-      $scope.items.splice(idx, 1);
-      localStorage.setItem("grocery", JSON.stringify($scope.items));
+      $scope.alldata.results.splice(idx, 1);
+      localStorage.setItem("grocery", JSON.stringify($scope.alldata));
+      $scope.updateGraphs();
     };
 
 
